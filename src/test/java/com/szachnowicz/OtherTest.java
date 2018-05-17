541ff51208a0403ec9786c8c3c7439aa10448f1a
Sebasitan Szachnowicz <sebastianszachnowicz@gmail.com>
2018-05-17 19:53:28 +0200

    commit with update files

---
+++ src/test/java/com/szachnowicz/OtherTest.java
@@ -0,0 +1,13 @@
+package com.szachnowicz;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OtherTest {
+
+    @Test
+    public void simpleRandomTest4() {
+        assertTrue(0 % 4 == 0);
+    }
+}
